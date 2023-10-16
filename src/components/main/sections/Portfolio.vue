1d6fc979e363a30644f1f39e15a93d2df989ed89
stefansimereaa <stefanosimerea16@gmail.com>
2023-10-16 19:37:51 +0200

    added show more button

--- src/components/main/sections/Portfolio.vue
+++ src/components/main/sections/Portfolio.vue
@@ -1,9 +1,24 @@
 <script>
-  // Exported component
 export default {
+  // Exported Component
   name: 'Portfolio',
+  
+  mounted() {
+    // Actions Showmore btn
+    const showMoreButton = document.getElementById('showMoreButton');
+    const hiddenCards = document.querySelectorAll('.card.d-none');
+
+    showMoreButton.addEventListener('click', () => {
+      hiddenCards.forEach((card) => {
+        card.classList.remove('d-none');
+      });
+      // Action for display none btn 
+      showMoreButton.style.display = 'none'; 
+    });
+  },
 }
 </script>
+
 <template>
   <section id="portfolio">
     <div class="container">
@@ -87,6 +102,63 @@ export default {
             </div>
           </div>
         </div>
+
+        <!-- Project 4 -->
+        <div class="card card-normal mb-5 d-none">
+          <!-- Link project -->
+          <div class="col-6">
+            <a href="">
+              <img src="../../../assets/img/projects/discord.png" alt="website">
+            </a>
+          </div>
+          <div class="col-5 d-flex flex-column justify-content-center align-items-center">
+            <div class="container d-flex flex-column justify-content-center align-items-center">
+              <h3>Discord <span class="date-time">(April 2023)</span></h3>
+              <p class="description-project">A Discord bot mockup illustrates the interface of a virtual assistant designed to enhance Discord server functionality. It showcases commands, responses, and interactions, giving users a preview of the bot's capabilities, from managing tasks to providing information or moderating discussions</p>
+            </div>
+            <div class="stacks d-flex justify-content-center align-items-center">
+              <p>Html</p>
+              <p>Css</p>
+            </div>
+            <div class="links d-flex align-items-center justify-content-center">
+              <a href="https://github.com/stefansimereaa/htmlcss-discord" target="_blank" class="me-4">Code 
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-brand-github" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: currentColor;"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
+              <a href="#">Live Demo 
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-external-link" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: currentColor;"><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path><path d="M11 13l9 -9"></path><path d="M15 4h5v5"></path></svg></a>
+            </div>
+          </div>
+        </div>
+
+        <!-- Project 5 -->
+        <div class="card card-normal mb-5 d-none">
+          <!-- Link project -->
+          <div class="col-6">
+            <a href="">
+              <img src="../../../assets/img/projects/discord.png" alt="website">
+            </a>
+          </div>
+          <div class="col-5 d-flex flex-column justify-content-center align-items-center">
+            <div class="container d-flex flex-column justify-content-center align-items-center">
+              <h3>Discord <span class="date-time">(April 2023)</span></h3>
+              <p class="description-project">A Discord bot mockup illustrates the interface of a virtual assistant designed to enhance Discord server functionality. It showcases commands, responses, and interactions, giving users a preview of the bot's capabilities, from managing tasks to providing information or moderating discussions</p>
+            </div>
+            <div class="stacks d-flex justify-content-center align-items-center">
+              <p>Html</p>
+              <p>Css</p>
+            </div>
+            <div class="links d-flex align-items-center justify-content-center">
+              <a href="https://github.com/stefansimereaa/htmlcss-discord" target="_blank" class="me-4">Code 
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-brand-github" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: currentColor;"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
+              <a href="#">Live Demo 
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-external-link" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: currentColor;"><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path><path d="M11 13l9 -9"></path><path d="M15 4h5v5"></path></svg></a>
+            </div>
+          </div>
+        </div>
+
+        
+
+        <!-- Aggiungi il pulsante "Mostra di più" -->
+        <button id="showMoreButton" class="btn btn-primary">Mostra di più</button>
       </div>
     </div>
   </section>
