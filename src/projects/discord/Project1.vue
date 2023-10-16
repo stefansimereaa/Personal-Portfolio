108a4b1f5e02a36bd5de78515bcf78335aeb6ce6
stefansimereaa <stefanosimerea16@gmail.com>
2023-10-16 20:19:06 +0200

    added discord mockup

---
+++ src/projects/discord/Project1.vue
@@ -0,0 +1,522 @@
+<script>
+
+</script>
+<template>
+  <!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Struttura Discord</title>
+</head>
+<body>
+    
+    <!-- Header -->
+    <header>
+        <nav id="navigation-menu">
+            <div class="logo"><img src="img/logo.svg" alt=""></div>
+            <div class="link">
+                <ul class="header-list">
+                    <li class="header-element"><a href="#">Scarica</a></li>
+                    <li class="header-element"><a href="#">Perchè Discord</a></li>
+                    <li class="header-element"><a href="#">Nitro</a></li>
+                    <li class="header-element"><a href="#">Sicurezza</a></li>
+                    <li class="header-element"><a href="#">Assistenza</a></li>
+                </ul>
+            </div>
+            <div class="login">
+                <button>Accedi</button>
+            </div>
+        </nav>
+        <div class="nav-content">
+            <h1>Il tuo posto per parlare</h1>
+            <p>Che tu faccia parte di un club scolastico, gruppo di gamer, comunità artistica mondiale, o semplicemente di un piccolo gruppo di amici che vuole passare il tempo assieme, Discord rende piu facile chiacchierare ogni giorno e ritrovarsi più spesso.</p>
+            <div class="buttons-nav">
+                <button class="buttons-download"><i class="fa-solid fa-down-long"></i>   Scarica per mac</button>
+                <button class="buttons-download button-black">Apri Discord nel tuo Browser</button>
+            </div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+    <main>
+        <!-- Section 1 -->
+        <section class="section1">
+            <div class="section1-img">
+                <img src="img/item1.svg" alt="study-group">
+            </div>
+            <div class="section1-content">
+                <h1 class="section-title">Un luogo a invito con tutto lo spazio necessario per parlare</h1>
+                <p class="section-text">I server Discord sono organizzati in canali tematici in cui puoi collaborare, condividere o semplicemente parlare della tua giornata senza intasare una chat di gruppo.</p>
+                </div>
+        </section>
+         <!-- Section 2 -->
+        <div class="wave"></div>
+        <section class="section2">
+            <div class="section2-content">
+                <h1 class="section-title">Dove trovarsi è facile</h1>
+                <p class="section-text">Mettiti comodo in un canale vocale quando hai tempo. Gli amici nel tuo server possono vedere che sei li e unirsi instantaneamente senza dover chiamare.</p>
+            </div>
+            <div class="section2-img">
+                <img src="img/item2.svg" alt="voice-connected">
+            </div>
+        </section>   
+        <div class="wave upside-down"></div>
+
+
+        <!-- Section 3 -->
+        <section class="section3">
+            <div class="section3-img">
+                <img src="img/item3.svg" alt="Community">
+            </div>
+            <div class="section3-content">
+                <h1 class="section-title">Da pochi a un fandom</h1>
+                <p class="section-text">Dai inizio a una community di qualsiasi dimensione con strumenti di moderazione e accesso ai membri personalizzabile. Concedi ai membri poteri speciali, crea canali privati, e molto altro.</p>
+            </div>
+        </section> 
+
+
+        <!-- Section 4 -->
+        <div class="wave"></div>
+        <section class="section4">
+            <div class="section4-content">
+                <h1 class="section-title-bottom">Tecnologia affidabile per tenersi in contatto</h1>
+                <p class="section-text">Video e audio a bassa latenza ti faranno sentire come se foste tutti nella stessa stanza. Fai ciao in video, guarda gli amici giocare in streaming, o riunitevi in una sessione di disegno con la condivisione schermo.</p>
+            </div>
+            <div class="section4-img">
+                <img src="img/item4-big.svg" alt="video-call">
+            </div>
+        <div class="stars">
+            <div class="bottom-download">
+                <h2>Pronto a iniziare la tua avventura?</h2>
+                <button class="button-pink"><i class="fa-solid fa-down-long"></i> Scarica per mac</button>
+            </div>
+        </div>
+        </section>     
+    </main>
+
+    <!-- Footer -->
+    <footer>
+        <div class="main-content-footer">
+            <div class="footer-med-content">
+                <h2 class="footer-title-med">Il tuo posto per parlare</h2>
+                <div class="language">
+                    <img src="img/ita.png" alt="italian-language">
+                    <h4>Italiano <i class="fa-solid fa-angle-down"></i></h4>
+
+                </div>
+                    <div class="socials">
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-youtube"></i>
+                    </div>
+            </div>
+
+            <div class="footer-small-content">
+                <ul class="footer-list">
+                    <li class="footer-title">Prodotto</li>
+                    <li>Scarica</li>
+                    <li>Perchè Discord</li>
+                    <li>Nitro</li>
+                    <li>Sicurezza</li>
+                    <li>Assistenza</li>
+                </ul>
+            </div>
+            <div class="footer-small-content">
+                <ul class="footer-list">
+                    <li class="footer-title">Compagnia</li>
+                    <li>Chi siamo</li>
+                    <li>Lavora con noi</li>
+                    <li>Marchio</li>
+                    <li>Redazione</li>
+                    <li>Negozio</li>
+                </ul>
+            </div>
+            <div class="footer-small-content">
+                <ul>
+                    <li class="footer-title">Risorse</li>
+                    <li>Scarica</li>
+                    <li>Perchè Discord</li>
+                    <li>Nitro</li>
+                    <li>Sicurezza</li>
+                    <li>Assistenza</li>
+                    <li>Scarica</li>
+                    <li>Perchè Discord</li>
+                    <li>Nitro</li>
+                    <li>Sicurezza</li>
+                    <li>Assistenza</li>
+                    <li>Assistenza</li>
+                </ul>
+            </div>
+            <div class="footer-small-content">
+                <ul class="footer-list">
+                    <li class="footer-title">Norme</li>
+                    <li>Scarica</li>
+                    <li>Perchè Discord</li>
+                    <li>Nitro</li>
+                    <li>Sicurezza</li>
+                    <li>Assistenza</li>
+                </ul>
+            </div>
+        </div>
+        <div class="line-footer"></div>    
+        <div class="last-content-footer">
+            <div class="content">
+                <div class="logo-footer">
+                    <img src="img/logo.svg" alt="logo">
+                </div>
+                <div class="button-footer">
+                    <button class="button-pink-footer">Registrati</button>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
+</template>
+<style>
+  /* Reset */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+/* Colors*/
+:root {
+    --footer: #23272a ;
+    --gray:#e5e5e5;
+}
+
+/* Generics */
+body{
+    font-family: Whitney, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+}
+.section-title{
+    font-size: 40px;
+    margin-bottom: 20px;
+}
+
+.section-text{
+    padding: 0;
+}
+
+/* Waves */
+.wave{
+    background-image: url('../img/wave.svg');
+    height: 100px;
+    display: block;
+    opacity: 0.1;
+}
+.upside-down{
+    rotate: 0.5turn;
+}
+
+
+
+/*Navbar */
+
+header{
+    background-color: var(--Header-background);
+    height: 650px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-image:url(../img/jumbo.png);
+    background-size: cover;
+    flex-wrap: wrap;
+}
+
+#navigation-menu {
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 1000px ;
+    margin-top: 10px;
+    
+}
+.logo{
+    
+    width: 200px;
+    padding:  10px 10px;
+    height: 50px;
+}
+.link{
+    width: 600px;
+    padding:  10px 10px;
+    text-align: center;
+    color: white;
+    display: flex;
+    flex-direction: row;
+}
+.header-list {
+    list-style-type: none;
+}
+.header-element{
+    display: inline-block;
+}
+a{
+    color: white;
+    text-decoration: none;
+    padding: 10px;
+}
+.login{
+    width: 200px;
+    padding:  10px 10px;
+    text-align: right;
+}
+button{
+    border-radius: 20px;
+    padding: 10px;
+    width: 80px;
+    border: none;
+}
+
+
+
+/*Navbar content*/
+
+
+.nav-content{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    flex-direction: column;
+    color: white;
+    height: 400px;
+    width: 700px;
+}
+p{
+    padding: 30px;
+    line-height: 32px;
+    text-align: center;
+}
+.buttons-nav{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+.buttons-download{
+    width: 200px;
+    height: 50px;
+    padding: 10px;
+    margin-left: 20px;
+}
+.button-black{
+    background-color: black;
+    color: white;
+    width: 300px;
+}
+
+
+
+/* Section1 and Section3 */
+
+.section1 , .section3{
+    display: flex;
+    justify-content: center;
+    height: 500px;
+    align-items: center;
+}
+
+.section1-img, .section3-img{
+    height: 400px;
+    display: flex;
+    padding: 0 10px;
+}
+
+.section1-content, .section3-content{
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    height: 400px;
+    margin-top: 30px;
+}
+
+
+/* Section2 */
+
+.section2{
+    display: flex;
+    justify-content: center;
+    height: 500px;
+    align-items: center;
+    background-color: var(--gray);
+}
+
+.section2-img{
+    height: 400px;
+    display: flex;
+    padding: 0px 10px;
+}
+
+.section2-content{
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    height: 400px;
+    margin-top: 0px;
+    padding: 20px;
+}
+
+
+
+/* Section4 */
+
+.section4{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    height: 1000px;
+    background-color: var(--gray);
+}
+.section4-img{
+    height: 500px;
+    margin-bottom: 40px;
+}
+
+.section4-content{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 800px;
+    height: 150px;
+    margin-top: 30px;
+}
+.section-title-bottom{
+    font-size: 35px;
+    margin-bottom: 20px;
+}
+
+.bottom-download{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 20px;
+    margin-bottom: 30px;
+    height: 200px;
+}
+
+.button-pink{
+    background-color: #8fa1e1;
+    color: white;
+    width: 200px;
+    height: 50px;
+    border-radius: 20px;
+    padding: 10px;
+    margin-top: 20px;
+}
+.stars{
+    background-image: url(../img/stars.svg);
+    height: 50px;
+    display: block;
+}
+
+
+/* Foooter */
+
+footer{
+    background-color: var(--footer);
+    height: 0 auto;
+}
+.main-content-footer{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 20px 20px;
+}
+.footer-med-content{
+    height: 350px;
+    width: 300px;
+}
+
+.footer-title-med {
+    color: #8fa1e1;
+    font-size: 30px;
+    padding: 20px 0px;
+}
+
+.footer-title{
+    color: #8fa1e1;
+}
+.language{
+    color: white;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 50px;
+}
+h4{
+    padding: 10px;
+}
+
+.socials{
+    color: white;
+    margin-top: 20px;
+}
+i{
+    padding: 10px;
+}
+
+.footer-small-content{
+    height: 350px;
+    width: 150px;
+    color: white;
+    padding: 10px;
+}
+
+ul{
+    list-style-type: none;
+}
+
+li{
+    padding: 5px;
+}
+
+.line-footer{
+    background-color: #8fa1e1;
+    height: 2px;
+    width: 1000px;
+    margin: 0 auto;
+}
+
+.last-content-footer{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 10px;
+}
+
+.content{
+    width: 1000px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+}
+.logo-footer{
+    width: 100px;
+    height: 30px;
+    padding: 0 10px;
+}
+.button-pink-footer{
+    width: 100px;
+    height: 30px;
+    padding: 0 10px;
+    background-color: #8fa1e1;
+    color: white;
+}
+
+</style>
