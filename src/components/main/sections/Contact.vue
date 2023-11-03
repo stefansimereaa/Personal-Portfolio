568c4a4f4ef026669ac2e97963e49fb60c2bb0bb
stefansimereaa <stefanosimerea16@gmail.com>
2023-11-03 12:56:17 +0100

    added email contact link

--- src/components/main/sections/Contact.vue
+++ src/components/main/sections/Contact.vue
@@ -69,7 +69,11 @@ export default {
           <div class="contact-email d-flex align-items-center ">
             <!-- Icon Email -->
             <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-mail"><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path><path d="M3 7l9 6l9 -6"></path></svg>
+              <!-- Italian Version -->
+              <!-- English Version -->
+              <a class="color-hover" target="_blank" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=stefanosimerea15@gmail.com">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-mail"><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path><path d="M3 7l9 6l9 -6"></path></svg>
+              </a>
             </span>
             <!-- Info Email -->
             <div class="contact-info ms-3">
