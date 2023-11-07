d92f48843ae22b20e98b31c92f6ed032cbe7232a
stefansimereaa <stefanosimerea16@gmail.com>
2023-11-07 11:51:21 +0100

    added corporate project

--- src/components/main/sections/Portfolio.vue
+++ src/components/main/sections/Portfolio.vue
@@ -55,11 +55,13 @@ export default {
         <h3 :style="{ color: isDarkText ? '#fff' : 'inherit' }" :class="{ 'd-none': $store.state.isItalian }">Each project is a unique piece of development 🧩</h3>
       </div>
       <div class="portfolio-projects">
+
+        <!-- DISCORD -->
         <!-- Project 1 -->
         <div class="card card-normal mb-5" :style="[navbarStyle, navbarStyle.backgroundColor === '#181a1e' ? { 'box-shadow': '2px 2px 4px rgba(0,0,0,0.9)' } : {}]">
           <!-- Link project -->
           <div class="col-6">
-            <a href="https://proj-html-vuejs-ruby.vercel.app/" target="_blank">
+            <a href="https://discord-mockup.vercel.app/" target="_blank">
               <img src="../../../assets/img/projects/discord.png" alt="website">
             </a>
           </div>
@@ -83,42 +85,43 @@ export default {
             <div class="links d-flex align-items-center justify-content-center" :style="{ color: isDarkText ? '#fff' : 'inherit' }">
               <a href="https://github.com/stefansimereaa/htmlcss-discord" target="_blank" class="me-4">Code 
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-brand-github" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: currentColor;"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
-              <a href="https://proj-html-vuejs-ruby.vercel.app/" target="_blank">Live Demo 
+              <a href="https://discord-mockup.vercel.app/" target="_blank">Live Demo 
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-external-link" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: currentColor;"><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path><path d="M11 13l9 -9"></path><path d="M15 4h5v5"></path></svg></a>
             </div>
           </div>
         </div>
 
+        <!-- CORPORATE LANDING -->
         <!-- Project 2 -->
         <div class="card card-normal mb-5" :style="[navbarStyle, navbarStyle.backgroundColor === '#181a1e' ? { 'box-shadow': '2px 2px 4px rgba(0,0,0,0.9)' } : {}]">
           <!-- Link project -->
           <div class="col-5 d-flex flex-column justify-content-center align-items-center">
             <div class="container d-flex flex-column justify-content-center align-items-center">
-              <h3>Discord
+              <h3>Corporate
                 <!-- Italian  Version -->
-                <span class="date-time" :style="{ color: isDarkText ? '#fff' : 'inherit' }" :class="{ 'd-none': !$store.state.isItalian }">(Aprile 2023)</span>
+                <span class="date-time" :style="{ color: isDarkText ? '#fff' : 'inherit' }" :class="{ 'd-none': !$store.state.isItalian }">(Luglio 2023)</span>
                 <!-- English Version -->
-                <span class="date-time" :style="{ color: isDarkText ? '#fff' : 'inherit' }" :class="{ 'd-none': $store.state.isItalian }">(April 2023)</span>
+                <span class="date-time" :style="{ color: isDarkText ? '#fff' : 'inherit' }" :class="{ 'd-none': $store.state.isItalian }">(July 2023)</span>
               </h3>
               <!-- Italian Version -->
-              <p class="description-project" :style="{ color: isDarkText ? '#fff' : 'inherit' }" :class="{ 'd-none': !$store.state.isItalian }">Un mockup di Discord illustra l'interfaccia di un assistente virtuale progettato per migliorare le funzionalità del server Discord. Mostra comandi, risposte e interazioni, dando agli utenti un'anteprima delle capacità del bot, dalla gestione delle attività alla fornitura di informazioni o alla moderazione delle discussioni</p>
+              <p class="description-project" :style="{ color: isDarkText ? '#fff' : 'inherit' }" :class="{ 'd-none': !$store.state.isItalian }">Il sito web aziendale offre un'interfaccia virtuale completa che rappresenta il lavoro svolto dall'azienda nei settori del business planning, della tecnologia digitale e delle soluzioni moderne. Attraverso questa piattaforma, gli utenti possono esplorare le competenze e le capacità dell'azienda. Il sito web mostra chiaramente comandi, risposte e interazioni, dando una visione dettagliata delle soluzioni proposte.</p>
               <!-- English Version -->
-              <p class="description-project" :style="{ color: isDarkText ? '#fff' : 'inherit' }" :class="{ 'd-none': $store.state.isItalian }">A Discord mockup illustrates the interface of a virtual assistant designed to enhance Discord server functionality. It showcases commands, responses, and interactions, giving users a preview of the bot's capabilities, from managing tasks to providing information or moderating discussions</p>
+              <p class="description-project" :style="{ color: isDarkText ? '#fff' : 'inherit' }" :class="{ 'd-none': $store.state.isItalian }">The corporate website provides a comprehensive virtual interface that showcases the work carried out by the company in the fields of business planning, digital technology, and modern solutions. The website clearly displays commands, responses, and interactions, providing a detailed insight into the proposed solutions.</p>
             </div>
             <div class="stacks d-flex justify-content-center align-items-center">
               <p :style="[navbarStyle, navbarStyle.backgroundColor === '#181a1e' ? { 'box-shadow': '2px 2px 4px rgba(0,0,0,0.9)' } : {}]">Html</p>
               <p :style="[navbarStyle, navbarStyle.backgroundColor === '#181a1e' ? { 'box-shadow': '2px 2px 4px rgba(0,0,0,0.9)' } : {}]">Css</p>
             </div>
             <div class="links d-flex align-items-center justify-content-center" :style="{ color: isDarkText ? '#fff' : 'inherit' }">
-              <a href="https://github.com/stefansimereaa/htmlcss-discord" target="_blank" class="me-4">Code 
+              <a href="https://github.com/stefansimereaa/proj-html-vuejs" target="_blank" class="me-4">Code 
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-brand-github" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: currentColor;"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg></a>
-              <a href="/discord" target="_blank">Live Demo 
+              <a href="https://corporate-landing-project.vercel.app/" target="_blank">Live Demo 
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-external-link" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: currentColor;"><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path><path d="M11 13l9 -9"></path><path d="M15 4h5v5"></path></svg></a>
             </div>
           </div>
-          <div class="col-6">
-            <a href="/discord" target="_blank">
-              <img src="../../../assets/img/projects/discord.png" alt="website">
+          <div class="col-6 project-2">
+            <a href="https://corporate-landing-project.vercel.app/" target="_blank">
+              <img src="../../../assets/img/projects/corporatelanding.png" alt="website">
             </a>
           </div>
         </div>
@@ -339,6 +342,13 @@ export default {
 .card .col-6:hover img {
   transform: translateY(-69%);
 }
+
+.card .col-6:hover img {
+  transform: translateY(-90%);
+}
+
+
+
 .card-normal{
   flex-direction: row;
   gap: 2rem;
@@ -1131,6 +1141,11 @@ h3{
 .card .col-6:hover img {
   transform: translateY(-67%);
 }
+
+.card .project-2:hover img {
+  transform: translateY(-80%);
+}
+
 .card-normal{
   display: flex;
   gap: 100px;
